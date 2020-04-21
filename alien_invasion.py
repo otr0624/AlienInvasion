7d02e5023912cc7b84bd6ea9793d6b372d37dfd3
otr0624 <otr0624@mail.com>
2020-04-21 15:27:07 -0400

    Added bullet number limit logic

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,7 +39,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets))
 
             self._update_screen()
 
@@ -73,8 +72,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
