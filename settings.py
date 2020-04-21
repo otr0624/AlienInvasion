d1642a682fab68db49ab724eabb49d6924ff6807
otr0624 <otr0624@mail.com>
2020-04-21 13:56:41 -0400

    Modified ship and settings files to draw ship, and reduced window size

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         # Screen settings
-        self.screen_width = 1366
-        self.screen_height = 788
+        self.screen_width = 1200
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
