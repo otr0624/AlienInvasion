eb820884a57f098dbb23d108989f576986816c99
otr0624 <otr0624@mail.com>
2020-04-22 17:36:51 -0400

    Added increased points by level, rounding, comma separation, and score refinement

--- settings.py
+++ settings.py
@@ -26,6 +26,9 @@ class Settings:
         # How quickly game speeds up
         self.speedup_scale = 1.1
 
+        # How quickly point values increase
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -40,7 +43,8 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings"""
+        """Increase speed settings and point values"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
