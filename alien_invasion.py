9e16ef51eddb8b4d2de2c99e61b434e49383a225
otr0624 <otr0624@mail.com>
2020-04-22 13:09:56 -0400

    Added logic to lose ship if aliens hit bottom

--- alien_invasion.py
+++ alien_invasion.py
@@ -138,6 +138,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen
+        self._check_aliens_bottom()
+
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge"""
         for alien in self.aliens.sprites():
@@ -166,6 +169,15 @@ class AlienInvasion:
 
         # Pause
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen"""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit
+                self._ship_hit()
+                break
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
