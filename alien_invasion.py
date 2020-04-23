79a55c4c8a391e461e610e2fac52606924a1e10c
otr0624 <otr0624@mail.com>
2020-04-23 10:30:08 -0400

    Added high score display and calculation and fixed score reset bug

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,6 +43,7 @@ class AlienInvasion:
         # Make the Play button
         self.play_button = Button(self, "Play")
 
+
     def run_game(self):
         """Start the main loop for the game"""
         while True:
@@ -135,6 +136,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create a new fleet
--- game_stats.py
+++ game_stats.py
@@ -9,7 +9,11 @@ class GameStats:
         self.game_active = False
         self.score = 0
 
+        # High score should never be reset
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during a game"""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
 
